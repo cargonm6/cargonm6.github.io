4f8dbee36e5143e6d86984a4a1c4b6a978f68ea3
cargonm6 <cargonm6@teleco.upv.es>
2023-12-04 19:35:26 +0100

    Cambios estéticos y mejoras en el funcionamiento

--- pages/cfd_test/js/app.js
+++ pages/cfd_test/js/app.js
@@ -1,163 +1,206 @@
+// variables globales
 let score = 0;
-var questions;
-randomize = true;
+let questions;
+let randomize = true;
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("cuestionarios").value = "none";
-    document.getElementById("checkrandom").checked = true;
+  // Inicializa el selector a "none" y el checkbox a true
+  document.getElementById("cuestionarios").value = "none";
+  document.getElementById("checkrandom").checked = true;
 });
 
 function changeRandom(check) {
-    randomize = check.checked;
+  // Cambia el estado de la variable
+  randomize = check.checked;
+
+  // Recarga el JSON abierto
+  if (document.getElementById("cuestionarios").value != "none") {
+    changeJSON(document.getElementById("cuestionarios"));
+  }
 }
 
 function changeJSON(sel) {
-    document.getElementById("result-box").style = "display: block";
+  // Muestra el bloque de respuesta
+  document.getElementById("result-box").style = "display: block";
 
-    let return_link = document.createElement("a");
-    return_link.innerHTML = '<i class="fa-solid fa-house"></i>&nbsp;Volver al inicio';
-    return_link.href = "JavaScript: location.reload(true);";
+  // Añade el bloque para volver al inicio (reiniciar)
+  let return_link = document.createElement("a");
+  return_link.innerHTML =
+    '<i class="fa-solid fa-house"></i>&nbsp;Volver al inicio';
+  return_link.href = "JavaScript: location.reload(true);";
 
-    let container = document.getElementById("app");
-    container.innerHTML = "";
-    container.appendChild(return_link);
+  let print_link = document.createElement("a");
+  print_link.style.float = "right";
+  print_link.innerHTML = '<i class="fa-solid fa-print"></i>&nbsp;Imprimir';
+  print_link.href = "javascript:void(0);";
+  // Adjuntar un evento de clic al enlace para imprimir la página
+  print_link.addEventListener("click", function () {
+    window.print();
+  });
 
-    fetch("json/" + sel.value + ".json")
-        .then((response) => response.json())
-        .then((data) => {
-            // Barajar aleatoriamente las preguntas
-            data.preguntas = shuffleArray(data.preguntas);
-            renderQuestions(data.preguntas);
-            restore();
-        })
-        .catch((error) => console.error("Error fetching data:", error));
+  // Vacía el contenedor de cuestionarios
+  let container = document.getElementById("app");
+  container.innerHTML = "";
+
+  let div_links = document.createElement("div");
+  div_links.id = "div_links";
+  div_links.style.display = "block";
+  div_links.appendChild(return_link);
+  div_links.appendChild(print_link);
+  container.appendChild(div_links);
+
+  // Carga el JSON en función del valor del selector
+  fetch("json/" + sel.value + ".json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Barajar aleatoriamente las preguntas
+      data.preguntas = shuffleArray(data.preguntas);
+      renderQuestions(data.preguntas);
+      restore();
+    })
+    .catch((error) => console.error("Error fetching data:", error));
 }
 
 function renderQuestions(data) {
-    const appContainer = document.getElementById("app");
-    questions = data;
-    questions.forEach((question, index) => {
-        // Barajar aleatoriamente las respuestas
-        question.respuestas = shuffleArray(question.respuestas);
+  const appContainer = document.getElementById("app");
+  questions = data;
+  questions.forEach((question, index) => {
+    // Barajar aleatoriamente las respuestas
+    question.respuestas = shuffleArray(question.respuestas);
 
-        const questionContainer = document.createElement("div");
-        questionContainer.className = "question-container";
+    const questionContainer = document.createElement("div");
+    questionContainer.className = "question-container";
 
-        const questionText = document.createElement("p");
-        questionText.textContent = `${index + 1}. ${question.enunciado}`;
-        questionContainer.appendChild(questionText);
+    const questionText = document.createElement("p");
+    questionText.textContent = `${index + 1}. ${question.enunciado}`;
+    questionContainer.appendChild(questionText);
 
-        question.respuestas.forEach((answer) => {
-            const input = document.createElement(question.multiple ? "input" : "input");
-            input.type = question.multiple ? "checkbox" : "radio";
-            input.name = `question_${index}`;
-            input.value = answer.opcion;
-            input.id = `${index}_${answer.opcion}`;
+    question.respuestas.forEach((answer) => {
+      const input = document.createElement(
+        question.multiple ? "input" : "input"
+      );
+      input.type = question.multiple ? "checkbox" : "radio";
+      input.name = `question_${index}`;
+      input.value = answer.opcion;
+      input.id = `${index}_${answer.opcion}`;
 
-            const label = document.createElement("label");
-            label.textContent = answer.opcion;
-            label.htmlFor = `${index}_${answer.opcion}`;
+      const label = document.createElement("label");
+      label.textContent = answer.opcion;
+      label.htmlFor = `${index}_${answer.opcion}`;
 
-            questionContainer.appendChild(input);
-            questionContainer.appendChild(label);
-            questionContainer.appendChild(document.createElement("br"));
-        });
-
-        appContainer.appendChild(questionContainer);
+      questionContainer.appendChild(input);
+      questionContainer.appendChild(label);
+      questionContainer.appendChild(document.createElement("br"));
     });
+
+    appContainer.appendChild(questionContainer);
+  });
 }
 
 // Función para barajar aleatoriamente un array
 function shuffleArray(array) {
-    if (randomize == true) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
+  if (randomize == true) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
-    return array;
+  }
+  return array;
 }
 
 function showResults() {
-    restore(false);
-    score = 0;
-    const questionContainers = document.querySelectorAll(".question-container");
+  restore(false);
+  score = 0;
+  const questionContainers = document.querySelectorAll(".question-container");
 
-    questionContainers.forEach((questionContainer, index) => {
-        const inputs = questionContainer.querySelectorAll("input");
-        const correctAnswers = getCorrectAnswers(questions[index]);
+  questionContainers.forEach((questionContainer, index) => {
+    const inputs = questionContainer.querySelectorAll("input");
+    const correctAnswers = getCorrectAnswers(questions[index]);
 
-        inputs.forEach((input) => {
-            const label = input.nextElementSibling;
+    inputs.forEach((input) => {
+      const label = input.nextElementSibling;
 
-            if (input.checked && correctAnswers.includes(input.value)) {
-                label.classList.add("correct-answer");
-            } else if (input.checked && !correctAnswers.includes(input.value)) {
-                label.classList.add("wrong-answer");
-            } else if (!input.checked && correctAnswers.includes(input.value)) {
-                label.classList.add("correct-answer");
-            }
-        });
-
-        const mark = document.createElement("span");
-
-        questionContainer.firstChild.appendChild(mark);
-
-        if (inputs.length > 0 && areAllCorrect(inputs, correctAnswers)) {
-            score += 1;
-            mark.className = "tick";
-            mark.textContent = " ✓";
-        } else {
-            mark.className = "cross";
-            mark.textContent = " ✗";
-        }
+      if (input.checked && correctAnswers.includes(input.value)) {
+        label.classList.add("correct-answer");
+      } else if (input.checked && !correctAnswers.includes(input.value)) {
+        label.classList.add("wrong-answer");
+      } else if (!input.checked && correctAnswers.includes(input.value)) {
+        label.classList.add("correct-answer");
+      }
     });
 
-    document.getElementById("result").innerHTML = `Puntuación: ${score}/${questionContainers.length}`;
-    document.getElementById("result").className = "";
-    if (score / questionContainers.length >= 0.5) {
-        document.getElementById("result").classList.add("tick");
+    const mark = document.createElement("span");
+
+    questionContainer.firstChild.appendChild(mark);
+
+    if (inputs.length > 0 && areAllCorrect(inputs, correctAnswers)) {
+      score += 1;
+      mark.className = "tick";
+      mark.textContent = " ✓";
     } else {
-        document.getElementById("result").classList.add("cross");
+      mark.className = "cross";
+      mark.textContent = " ✗";
     }
+  });
+
+  document.getElementById(
+    "result"
+  ).innerHTML = `Puntuación: ${score}/${questionContainers.length}`;
+  document.getElementById("result").className = "";
+  if (score / questionContainers.length >= 0.5) {
+    document.getElementById("result").classList.add("tick");
+  } else {
+    document.getElementById("result").classList.add("cross");
+  }
 }
 
 function restore(input = false) {
-    const questionContainers = document.querySelectorAll(".question-container");
-    document.getElementById("result").innerHTML = "";
+  // Vacía el contenedor de resultado
+  const questionContainers = document.querySelectorAll(".question-container");
+  document.getElementById("result").innerHTML = "";
 
-    questionContainers.forEach((questionContainer) => {
-        const inputs = questionContainer.querySelectorAll("input");
-        const labels = questionContainer.querySelectorAll("label");
-        const tick = questionContainer.firstChild.querySelector(".tick");
-        const cross = questionContainer.firstChild.querySelector(".cross");
+  // Por cada pregunta...
+  questionContainers.forEach((questionContainer) => {
+    // Si se debe restaurar el formulario completo, reinicia todos los checkbox
+    if (input == true) {
+      const inputs = questionContainer.querySelectorAll("input");
+      inputs.forEach((input) => {
+        input.checked = false;
+      });
+    }
 
-        if (input == true) {
-            inputs.forEach((input) => {
-                input.checked = false;
-            });
-        }
-
-        labels.forEach((label) => {
-            label.classList.remove("correct-answer", "wrong-answer");
-        });
-
-        if (tick) {
-            questionContainer.firstChild.removeChild(tick);
-        } else if (cross) {
-            questionContainer.firstChild.removeChild(cross);
-        }
+    // Retira si la respuesta es correcta o no
+    const labels = questionContainer.querySelectorAll("label");
+    labels.forEach((label) => {
+      label.classList.remove("correct-answer", "wrong-answer");
     });
+
+    // Elimina los símbolos de tick y cross
+    const tick = questionContainer.firstChild.querySelector(".tick");
+    const cross = questionContainer.firstChild.querySelector(".cross");
+    if (tick) {
+      questionContainer.firstChild.removeChild(tick);
+    } else if (cross) {
+      questionContainer.firstChild.removeChild(cross);
+    }
+  });
 }
 
 function getCorrectAnswers(question) {
-    return question.respuestas.filter((answer) => answer.verdadera).map((answer) => answer.opcion);
+  // Devuelve la relación de respuestas correctas
+  return question.respuestas
+    .filter((answer) => answer.verdadera)
+    .map((answer) => answer.opcion);
 }
 
 function areAllCorrect(inputs, correctAnswers) {
-    const selectedAnswers = Array.from(inputs)
-        .filter((input) => input.checked)
-        .map((input) => input.value);
+  // Indica si el número de aciertos es el número total de respuestas correctas de la pregunta
+  const selectedAnswers = Array.from(inputs)
+    .filter((input) => input.checked)
+    .map((input) => input.value);
 
-    return selectedAnswers.length === correctAnswers.length && selectedAnswers.every((answer) => correctAnswers.includes(answer));
+  return (
+    selectedAnswers.length === correctAnswers.length &&
+    selectedAnswers.every((answer) => correctAnswers.includes(answer))
+  );
 }
